208f712c5c86dd20327c6d9d43b93a8d6a60670a
ahaddinsharawi-lgtm <ahaddinsharawi@gmail.com>
2026-03-28 19:58:25 +0800

    Implement smooth scrolling on button click
    Add smooth scrolling behavior for dark button links

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+document.querySelector('.btn-dark').addEventListener('click', function(e) {
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+        e.preventDefault();
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+});
